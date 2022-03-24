141ebac0d2eae9c3fee4545f4e97faba06cf297d
Rodrigo <rodrigolpj@hotmail.com>
2022-03-24 18:37:55 -0300

    Removed wrong route

--- vue/teste_teknisa/src/router/index.js
+++ vue/teste_teknisa/src/router/index.js
@@ -1,15 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HeaderComponent from '@/components/HeaderComponent'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/header',
-      name: 'HeaderComponent',
-      component: HeaderComponent
-    }
+
   ]
 })
