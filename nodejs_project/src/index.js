17e4cb5aa2cc7e9fffd381a792de8afe687ec209
Rodrigo <rodrigolpj@hotmail.com>
2022-03-24 09:49:02 -0300

    Finished NodeJS project

--- nodejs_project/src/index.js
+++ nodejs_project/src/index.js
@@ -28,31 +28,119 @@ app.get('/syncDatabase', async (req, res) => {
 });
 
 app.post('/createProgrammer', async (req, res) => {
-try {
-    const params = req.body;
+    try {
+        const params = req.body;
 
-    const properties = ['name', 'python', 'java', 'javascript'];
+        const properties = ['name', 'python', 'java', 'javascript'];
 
-    const check = properties.every((property) => {
-    return property in params;
-    });
+        validateProperties(properties, params, 'every');
 
-    if (!check) {
-    const propStr = properties.join(', ');
-    res.send(`All parameters needed to create a programmer must be sent: ${propStr}`);
-    return;
+        const newProgrammer = await programmer.create({
+            name: params.name,
+            python: params.python,
+            javascript: params.javascript,
+            java: params.java
+        });
+
+        res.send(newProgrammer);
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+app.get('/retrieveProgrammer', async (req, res) => {
+    try {
+      const params = req.query;
+   
+      if ('id' in params) {
+        const record = await programmer.findByPk(params.id);
+   
+        if (record) {
+          res.send(record);
+        } else {
+          res.send('No programmer found using received ID');
+        }
+   
+        return;
+      }
+   
+      const records = await programmer.findAll();
+   
+      res.send(records);
+    } catch (error) {
+      res.send(error);
+    }
+  });
+
+  app.put('/updateProgrammer', async (req, res) => {
+    try {
+      const params = req.body;
+      
+      const record = await validateId(params);
+      
+      const properties = ['name', 'python', 'java', 'javascript'];
+   
+      validateProperties(properties, params, 'some');
+   
+      record.name = 'name' in params ? params.name : record.name;
+      record.python = 'python' in params ? params.python : record.python;
+      record.java = 'java' in params ? params.java : record.java;
+      record.javascript = 'javascript' in params ? params.javascript : record.javascript;
+   
+      await record.save();
+   
+      res.send(`${record.id} ${record.name} - Updated successfully`);
+    } catch (error) {
+      res.send(error);
+    }
+  });
+  
+  app.delete('/deleteProgrammer', async (req, res) => {
+    try {
+      const params = req.body;
+   
+      const record = await validateId(params); 
+   
+      await record.destroy();
+   
+      res.send(`${record.id} ${record.name} - Deleted successfully`);
+    } catch (error) {
+      res.send(error);
+    }
+  });
+
+  const validateId =  async (params) => {
+    try {
+      if (!('id' in params)) {
+        throw(`Missing 'id' in request body`);
+      }
+   
+      const record = await programmer.findByPk(params.id);
+   
+      if (!record) {
+        throw(`Programmer ID not found.`);
+      }
+
+      return record;
+
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  const validateProperties = (properties, params, fn) => {
+    try {
+      const check = properties[fn]((property) => {
+        return property in params;
+      });
+   
+      if (!check) {
+        const propStr = properties.join(', ');
+        throw(`Request body doesn't have any of the following properties: ${propStr}`);
+      }
+      return true;
+    } catch (error) {
+      throw error;
     }
 
-
-    const newProgrammer = await programmer.create({
-    name: params.name,
-    python: params.python,
-    javascript: params.javascript,
-    java: params.java
-    });
-
-    res.send(newProgrammer);
-} catch (error) {
-    res.send(error);
-}
-});
+  }
